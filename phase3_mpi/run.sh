4c3a1028281172f0a882c1c449392aad90b19fd5
NguyenDucHoang_SE200032 <hhprolay@gmail.com>
2026-04-16 22:17:15 +0700

    fix phase3_mpi and add docs for phase3

---
+++ phase3_mpi/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "========================================"
+echo "BẮT ĐẦU BIÊN DỊCH DỰ ÁN N-BODY (PHASE 3)"
+echo "========================================"
+
+mkdir -p build
+cd build
+rm -f nbody_sim
+cmake ..
+make
+
+if [ $? -eq 0 ]; then
+    echo "-------------------------------------------"
+    echo "BIÊN DỊCH THÀNH CÔNG! ĐANG CHẠY MÔ PHỎNG..."
+    echo "-------------------------------------------"
+    time ./nbody_sim
+else
+    echo "----------------------------------------------"
+    echo "LỖI BIÊN DỊCH! VUI LÒNG KIỂM TRA LẠI CODE C++."
+    echo "----------------------------------------------"
+fi
